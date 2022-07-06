f2433d46a0701c6e2c4e60d53c4491e7e50fbe3f
gauri23092002 <gauri23092002@gmail.com>
2022-07-06 12:58:45 +0530

    place searched

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TravelregistrationComponent } from './travelregistration/travelregistration.component';
+import { SearchplaceComponent } from './searchplace/searchplace.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TravelregistrationComponent
+    TravelregistrationComponent,
+    SearchplaceComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/searchplace/searchplace.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchplace',
+  templateUrl: './searchplace.component.html',
+  styleUrls: ['./searchplace.component.css']
+})
+export class SearchplaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
