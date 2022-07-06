268b287350bf50a93b2175a466ddac6b0b68ec4d
gauri23092002 <gauri23092002@gmail.com>
2022-07-06 12:43:09 +0530

    travel registered

---
+++ src/app/travelregistration/travelregistration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-travelregistration',
+  templateUrl: './travelregistration.component.html',
+  styleUrls: ['./travelregistration.component.css']
+})
+export class TravelregistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
